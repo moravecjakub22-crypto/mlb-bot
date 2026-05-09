d944dec29bc0253fe3f6194cc3eb28a296b3e542
moravecjakub22-crypto <moravec.jakub22@gmail.com>
2026-05-09 08:02:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -145,7 +145,7 @@ def main():
                     print(f"{home} vs {away} | inning {inning} | score {score}")
 
                     # 👉 jen silné situace
-                    if score < 6:
+                    if score < 7:
                         continue
 
                     # 🔄 ODDS update každých 30 min
